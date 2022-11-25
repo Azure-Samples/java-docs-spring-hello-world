b0d053de7bb5617db3163414b7c97debbe1f101f
DeepakKaligotla <65765904+deepakkaligotla@users.noreply.github.com>
2022-11-25 23:18:32 +0530

    Update DemoApplication.java
    TQ

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -30,6 +30,7 @@ import com.example.demo.Dao.SponsorDao;
 
 @SpringBootApplication
 @RestController
+@ComponentScan(basepackages="com.example")
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
