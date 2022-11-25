cf6e1c2827db2439245b649e9c081095d91b1c67
DeepakKaligotla <65765904+deepakkaligotla@users.noreply.github.com>
2022-11-25 23:30:03 +0530

    Update DemoApplication.java
    hmmm

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,6 +21,7 @@ import Entity.Sponsor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +31,7 @@ import com.example.demo.Dao.SponsorDao;
 
 @SpringBootApplication
 @RestController
-@ComponentScan(basepackages="com.example")
+@ComponentScan(basePackages="com.example")
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
