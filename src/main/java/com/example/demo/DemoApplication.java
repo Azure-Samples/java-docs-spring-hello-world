6d93a5eb93544a9d552a97eb3a9d80a10ecae1c7
Ranadip <jj152@users.noreply.github.com>
2023-04-10 18:53:56 +0530

    test commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello Azure App service12";
+		return "Hello Azure App service123";
 	}
 }
---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
