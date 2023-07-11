2e8e31aebab8d3922dd6f05ababeffb760465427
shivamsinghal1123 <127539395+shivamsinghal1123@users.noreply.github.com>
2023-07-11 23:08:11 +0530

    Code Change 3 DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,29 +1,11 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		int c = a + b;
-		System.out.println("The sum of a and b is " + c);
-		SpringApplication.run(DemoApplication.class, args);
+public class SumOfNumbers1
+	{
+		public static void main(String args[])
+		{
+			int n1 = 225, n2 = 115, sum;
+			sum = n1 + n2;
+			System.out.println("The sum of numbers is"+sum);
+		}
 	}
-
-	@RequestMapping("/")
-	String sayHello() {
-		return "Hello World, This is Azure talking to U!";
-	}
-
-	@RequestMapping("/")
-	String sayYes() {
-		return " The sum of a and b is 30 ";
-	}
-}
