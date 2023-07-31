0b9d785dae289228875807b15dde036dfd33cf9b
shankarpc07 <140762683+shankarpc07@users.noreply.github.com>
2023-07-31 17:08:30 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.*;
 
 @SpringBootApplication
 @RestController
@@ -11,10 +12,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		Connection connection = DriverManager.getConnection("jdbc:sqlserver://pricedb07.database.windows.net:1433;database=Priceit;user=dbadmin@pricedb07;password=Admin123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 	}
 
 	@RequestMapping("/")
 	String sayHello() {
 		return "Hello Shankar!";
+		
 	}
 }
