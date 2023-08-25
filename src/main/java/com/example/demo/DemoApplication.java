2e887b97386c681756274ce79902f289ac506398
yhjing <138578134+yhjing@users.noreply.github.com>
2023-08-25 15:04:44 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +14,33 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	String sayHello() {
-		return "Hello Azure!!";
+	// @RequestMapping("/")
+	// String sayHello() {
+	// 	return "Hello Azure!!";
+	// }
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
+		if (file.isEmpty()) {
+			model.addAttribute("message", "Please select a file to upload.");
+			return "index";
+		}
+
+		try {
+			// Handle file upload logic here
+			// You can use the file.getInputStream() to read the file data
+			// Save the file to your desired location or cloud storage
+
+			model.addAttribute("message", "File uploaded successfully.");
+		} catch (Exception e) {
+			model.addAttribute("message", "Error uploading file.");
+		}
+
+		return "index";
 	}
 }
